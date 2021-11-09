381a077a3de8151b428622b06480849e545accd0
Ajit Kumar <A.Kumar9@nuigalway.ie>
2021-11-09 20:35:45 +0000

    Add project files.

---
+++ Assignment3/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Assignment3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.passwordEntryPanel = new System.Windows.Forms.Panel();
+            this.passwordEntryButton = new System.Windows.Forms.Button();
+            this.passwordScreenImage = new System.Windows.Forms.PictureBox();
+            this.passwordEntryLabel = new System.Windows.Forms.Label();
+            this.passwordEntryTextBox = new System.Windows.Forms.TextBox();
+            this.passwordEntryPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.passwordScreenImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // passwordEntryPanel
+            // 
+            this.passwordEntryPanel.Controls.Add(this.passwordEntryButton);
+            this.passwordEntryPanel.Controls.Add(this.passwordScreenImage);
+            this.passwordEntryPanel.Controls.Add(this.passwordEntryLabel);
+            this.passwordEntryPanel.Controls.Add(this.passwordEntryTextBox);
+            this.passwordEntryPanel.Location = new System.Drawing.Point(0, 0);
+            this.passwordEntryPanel.Name = "passwordEntryPanel";
+            this.passwordEntryPanel.Size = new System.Drawing.Size(1575, 1131);
+            this.passwordEntryPanel.TabIndex = 0;
+            // 
+            // passwordEntryButton
+            // 
+            this.passwordEntryButton.Location = new System.Drawing.Point(713, 857);
+            this.passwordEntryButton.Name = "passwordEntryButton";
+            this.passwordEntryButton.Size = new System.Drawing.Size(150, 46);
+            this.passwordEntryButton.TabIndex = 3;
+            this.passwordEntryButton.Text = "&Proceed";
+            this.passwordEntryButton.UseVisualStyleBackColor = true;
+            this.passwordEntryButton.Click += new System.EventHandler(this.passwordEntryButton_Click);
+            // 
+            // passwordScreenImage
+            // 
+            this.passwordScreenImage.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("passwordScreenImage.BackgroundImage")));
+            this.passwordScreenImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.passwordScreenImage.Location = new System.Drawing.Point(413, 100);
+            this.passwordScreenImage.Name = "passwordScreenImage";
+            this.passwordScreenImage.Size = new System.Drawing.Size(750, 500);
+            this.passwordScreenImage.TabIndex = 2;
+            this.passwordScreenImage.TabStop = false;
+            // 
+            // passwordEntryLabel
+            // 
+            this.passwordEntryLabel.Location = new System.Drawing.Point(588, 721);
+            this.passwordEntryLabel.Name = "passwordEntryLabel";
+            this.passwordEntryLabel.Size = new System.Drawing.Size(400, 40);
+            this.passwordEntryLabel.TabIndex = 1;
+            this.passwordEntryLabel.Text = "Enter Password:";
+            // 
+            // passwordEntryTextBox
+            // 
+            this.passwordEntryTextBox.Location = new System.Drawing.Point(588, 765);
+            this.passwordEntryTextBox.Name = "passwordEntryTextBox";
+            this.passwordEntryTextBox.PasswordChar = '*';
+            this.passwordEntryTextBox.Size = new System.Drawing.Size(400, 39);
+            this.passwordEntryTextBox.TabIndex = 0;
+            this.passwordEntryTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.passwordEntryTextBox_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1574, 1129);
+            this.Controls.Add(this.passwordEntryPanel);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Form1";
+            this.passwordEntryPanel.ResumeLayout(false);
+            this.passwordEntryPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.passwordScreenImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel passwordEntryPanel;
+        private System.Windows.Forms.Label passwordEntryLabel;
+        private System.Windows.Forms.TextBox passwordEntryTextBox;
+        private System.Windows.Forms.PictureBox passwordScreenImage;
+        private System.Windows.Forms.Button passwordEntryButton;
+    }
+}
+
---
+++ Assignment3/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment3
+{
+    public partial class Form1 : Form
+    {
+        // Literal value - correct password
+        const string PASSWORD_VALUE = "ShowMeTheMoney";
+
+        // Literal value - limit for number of failed attempts to enter password
+        const int PASSWORD_FAILED_ATTEMPTS_LIMIT = 4;
+
+        // Literal values - wrong password
+        const string WRONG_PASSWORD_MESSAGE = "Wrong Password";
+        const string TOO_MANY_FAILED_ATTEMPTS_MESSAGE = "Too many failed attempts";
+        const string LOCKED_OUT_MESSAGE = "Locked out";
+
+        // Variable to keep track of number of failed attempts to enter password
+        int passwordAttemptsCounter = 0;
+
+        // Function to check is password is correct
+        Boolean isPasswordCorrect(String enteredPassword) => enteredPassword == PASSWORD_VALUE;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Method triggered on clicking the "Proceed" button on the first screen
+        private void passwordEntryButton_Click(object sender, EventArgs e)
+        {
+            // Obtain entered password
+            String enteredPassword = passwordEntryTextBox.Text;
+
+            // Check if entered password is correct
+            if (isPasswordCorrect(enteredPassword))
+            {
+                // If so, proceed to the next screen
+                passwordEntryPanel.Hide();
+            }
+            else
+            {
+                // Update counter for number of failed password entry attempts
+                passwordAttemptsCounter++;
+                // Check if we are within the limit for failed password attempts
+                if (passwordAttemptsCounter < PASSWORD_FAILED_ATTEMPTS_LIMIT)
+                    // Show message indicating that a wrong password is entered
+                    MessageBox.Show(WRONG_PASSWORD_MESSAGE);
+                else
+                {
+                    // Show message indicating that too many failed attempts have occured
+                    DialogResult result =
+                    MessageBox.Show(TOO_MANY_FAILED_ATTEMPTS_MESSAGE, LOCKED_OUT_MESSAGE, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    // Then exit the application
+                    if (result == DialogResult.OK)
+                        Application.Exit();
+                }
+
+            }
+        }
+
+        private void passwordEntryTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                passwordEntryButton.PerformClick();
+                e.SuppressKeyPress = true;
+                e.Handled = true;
+            }
+        }
+
+    }
+
+}
